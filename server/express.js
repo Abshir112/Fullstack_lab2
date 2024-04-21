f8085380fdfbc70ef5c179bff72cb0a772f7b734
abshir112 <abshir_muhumed.abdi0018@stud.hkr.se>
2024-04-21 11:03:45 +0200

    setup express server

---
+++ server/express.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static('dist'));
+
+// json parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
