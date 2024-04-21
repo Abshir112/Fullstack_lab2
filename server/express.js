49348b6f1b1012dcf663e8aa1ba327dacd4f0932
abshir112 <abshir_muhumed.abdi0018@stud.hkr.se>
2024-04-21 15:39:33 +0200

    serve endpoints

--- server/express.js
+++ server/express.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './router/router.js';
 
 dotenv.config();
 
@@ -12,8 +13,70 @@ app.use(express.static('dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// router
+app.use('/api', router);
+
+// // get all employee
+app.get('', async (req, res) => {
+    try {
+        const employee = await getAllEmployee();
+        res.status(200).json(employee);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
+
+// // Create a new employee
+// app.post('', async (req, res) => {
+//     const { employee_id, full_name, email, hashed_password } = req.body;
+//     try {
+//         const employee = await createEmployee(employee_id, full_name, email, hashed_password);
+//         res.status(201).json(employee);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// // get all project
+// app.get('', async (req, res) => {
+//     try {
+//         const project = await getAllProject();
+//         res.status(200).json(project);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// // Create a new project
+// app.post('', async (req, res) => {
+//     const { project_code, project_name, project_description } = req.body;
+//     try {
+//         const project = await createProject(project_code, project_name, project_description);
+//         res.status(201).json(project);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+   
+// // get all project assignment
+// app.get('', async (req, res) => {
+//     try {
+//         const projectAssignment = await getAllProjectAssignment();
+//         res.status(200).json(projectAssignment);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// // Create a new project assignment
+// app.post('', async (req, res) => {
+//     const { employee_id, project_code, start_date } = req.body;
+//     try {
+//         const projectAssignment = await createProjectAssignment(employee_id, project_code, start_date);
+//         res.status(201).json(projectAssignment);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
 
 export default app
--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import app from './express.js';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const port = process.env.PORT || 3000;
+
+//  Connect to the database
+mongoose.connect(process.env.MONGODB_URI, {})
+.then(() => { console.log('Connected to the database') })
+.catch((error) => { console.log('Failed to connect to the database', error) });
 
 
 app.listen(port, () => {
